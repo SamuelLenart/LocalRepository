42a035cb9c17923a87843fb61485467302336108
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 17:50:55 +0100

    Update local.java
    changed

--- local.java
+++ local.java
@@ -1,12 +1,19 @@
 public class local {
+    // hlavna metoda
     public static void main(String[] args){
+        // waw, ID
         ID person = new ID();
         System.out.println(person.checkId("976231/1240"));
     }
      if (ID.checkId()) {
         ID.Age();
-
+        // tu je komentar
         System.out.println("Den ked si sa narodil bol:");
 
+    }
+    
+    
+    public void hello(){
+      // vypis hello   
     }
 }
